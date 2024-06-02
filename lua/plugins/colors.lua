1031a3d84231efbbbd7a033a797cc1294cc4350d
Miro Paintola <miro.paintola@gmail.com>
2024-06-02 21:19:03 +0300

    new theme and some other things

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,14 +1,38 @@
 return {
-	{
-		"AlexvZyl/nordic.nvim",
-		lazy = false,
-		priority = 1000,
-		config = function()
-			require("nordic").setup({})
-			vim.cmd.colorscheme("nordic")
-		end,
-	},
+  "scottmckendry/cyberdream.nvim",
+  lazy = false,
+  priority = 1000,
+  config = function()
+    require("cyberdream").setup({
+      -- Enable transparent background
+      transparent = true,
+
+      -- Enable italics comments
+      italic_comments = true,
+
+      -- Replace all fillchars with ' ' for the ultimate clean look
+      hide_fillchars = true,
+
+      -- Modern borderless telescope theme
+      borderless_telescope = true,
+
+      -- Set terminal colors used in `:terminal`
+      terminal_colors = true,
+    })
+    vim.cmd("colorscheme cyberdream")
+  end
 }
+-- return {
+-- 	{
+-- 		"AlexvZyl/nordic.nvim",
+-- 		lazy = false,
+-- 		priority = 1000,
+-- 		config = function()
+-- 			require("nordic").setup({})
+-- 			vim.cmd.colorscheme("nordic")
+-- 		end,
+-- 	},
+-- }
 
 -- return {
 -- 	"folke/tokyonight.nvim",
@@ -24,45 +48,45 @@ return {
 -- }
 
 -- return {
--- 	"rebelot/kanagawa.nvim",
--- 	priority = 1000,
--- 	config = function()
--- 		require("kanagawa").setup({
--- 			theme = "wave",
--- 			commentStyle = { italic = true },
--- 			dimInactive = true,
--- 			-- transparent = true,
--- 			colors = {
--- 				theme = {
--- 					all = {
--- 						ui = {
--- 							bg_gutter = "none",
--- 							float = {
--- 								bg = "none",
--- 							},
--- 						},
--- 					},
--- 				},
--- 			},
--- 			-- overrides = function(colors)
--- 			-- 	local theme = colors.theme
--- 			-- 	return {
--- 			-- 		NormalFloat = { bg = "none" },
--- 			-- 		FloatBorder = { bg = "none" },
--- 			-- 		FloatTitle = { bg = "none" },
--- 			-- 		-- Save an hlgroup with dark background and dimmed foreground
--- 			-- 		-- so that you can use it where your still want darker windows.
--- 			-- 		-- E.g.: autocmd TermOpen * setlocal winhighlight=Normal:NormalDark
--- 			-- 		NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
--- 			--
--- 			-- 		-- Popular plugins that open floats will link to NormalFloat by default;
--- 			-- 		-- set their background accordingly if you wish to keep them dark and borderless
--- 			-- 		LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
--- 			-- 		MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
--- 			-- 	}
--- 			-- end,
--- 		})
--- 		vim.cmd("colorscheme kanagawa")
--- 		vim.wo.fillchars = "eob: " -- hides tildes from empty lines
--- 	end,
+--   "rebelot/kanagawa.nvim",
+--   priority = 1000,
+--   config = function()
+--     require("kanagawa").setup({
+--       theme = "wave",
+--       commentStyle = { italic = true },
+--       dimInactive = true,
+--       -- transparent = true,
+--       colors = {
+--         theme = {
+--           all = {
+--             ui = {
+--               bg_gutter = "none",
+--               float = {
+--                 bg = "none",
+--               },
+--             },
+--           },
+--         },
+--       },
+--       overrides = function(colors)
+--         local theme = colors.theme
+--         return {
+--           NormalFloat = { bg = "none" },
+--           FloatBorder = { bg = "none" },
+--           FloatTitle = { bg = "none" },
+--           -- Save an hlgroup with dark background and dimmed foreground
+--           -- so that you can use it where your still want darker windows.
+--           -- E.g.: autocmd TermOpen * setlocal winhighlight=Normal:NormalDark
+--           NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
+--
+--           -- Popular plugins that open floats will link to NormalFloat by default;
+--           -- set their background accordingly if you wish to keep them dark and borderless
+--           LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+--           MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+--         }
+--       end,
+--     })
+--     vim.cmd("colorscheme kanagawa")
+--     vim.wo.fillchars = "eob: " -- hides tildes from empty lines
+--   end,
 -- }
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,194 +1,195 @@
 return {
-	"neovim/nvim-lspconfig",
-	dependencies = {
-		"williamboman/mason.nvim",
-		"williamboman/mason-lspconfig.nvim",
-		"folke/neodev.nvim",
-		-- Autocompletion
-		{
-			"hrsh7th/nvim-cmp",
-			config = function()
-				local cmp = require("cmp")
-				local luasnip = require("luasnip")
-				local lspkind = require("lspkind")
+  "neovim/nvim-lspconfig",
+  version = "*",
+  dependencies = {
+    "williamboman/mason.nvim",
+    "williamboman/mason-lspconfig.nvim",
+    "folke/neodev.nvim",
+    -- Autocompletion
+    {
+      "hrsh7th/nvim-cmp",
+      config = function()
+        local cmp = require("cmp")
+        local luasnip = require("luasnip")
+        local lspkind = require("lspkind")
 
-				local has_words_before = function()
-					unpack = unpack or table.unpack
-					local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-					return col ~= 0
-						and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-				end
+        local has_words_before = function()
+          unpack = unpack or table.unpack
+          local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+          return col ~= 0
+              and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+        end
 
-				---@diagnostic disable-next-line missing-fields
-				cmp.setup({
-					snippet = {
-						expand = function(args)
-							luasnip.lsp_expand(args.body)
-						end,
-					},
-					sources = cmp.config.sources({
-						{ name = "nvim_lsp" },
-						{ name = "luasnip" },
-						{ name = "buffer" },
-						require("luasnip.loaders.from_vscode").lazy_load(),
-					}),
-					window = {
-						completion = cmp.config.window.bordered(),
-						documentation = cmp.config.window.bordered(),
-					},
-					---@diagnostic disable-next-line missing-fields
-					formatting = {
-						format = lspkind.cmp_format({
-							mode = "symbol_text",
-							maxwidth = 50,
-							ellipsis_char = "...",
-						}),
-					},
-					mapping = {
-						["<CR>"] = cmp.mapping.confirm({ select = true }),
-						["<C-g>"] = cmp.mapping.scroll_docs(-4),
-						["<C-b>"] = cmp.mapping.scroll_docs(4),
-						["<Up>"] = cmp.mapping.select_prev_item({ behavior = "select" }),
-						["<Down>"] = cmp.mapping.select_next_item({ behavior = "select" }),
-						["<C-Space>"] = cmp.mapping(function(fallback)
-							if cmp.visible() then
-								cmp.abort()
-							else
-								cmp.complete()
-							end
-						end),
-						["<Tab>"] = cmp.mapping(function(fallback)
-							if cmp.visible() then
-								cmp.select_next_item({ behavior = "insert" })
-							-- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
-							-- that way you will only jump inside the snippet region
-							elseif luasnip.expand_or_jumpable() then
-								luasnip.expand_or_jump()
-							elseif has_words_before() then
-								cmp.complete()
-							else
-								fallback()
-							end
-						end, { "i", "s" }),
+        ---@diagnostic disable-next-line missing-fields
+        cmp.setup({
+          snippet = {
+            expand = function(args)
+              luasnip.lsp_expand(args.body)
+            end,
+          },
+          sources = cmp.config.sources({
+            { name = "nvim_lsp" },
+            { name = "luasnip" },
+            { name = "buffer" },
+            require("luasnip.loaders.from_vscode").lazy_load(),
+          }),
+          window = {
+            completion = cmp.config.window.bordered(),
+            documentation = cmp.config.window.bordered(),
+          },
+          ---@diagnostic disable-next-line missing-fields
+          formatting = {
+            format = lspkind.cmp_format({
+              mode = "symbol_text",
+              maxwidth = 50,
+              ellipsis_char = "...",
+            }),
+          },
+          mapping = {
+            ["<CR>"] = cmp.mapping.confirm({ select = true }),
+            ["<C-g>"] = cmp.mapping.scroll_docs(-4),
+            ["<C-b>"] = cmp.mapping.scroll_docs(4),
+            ["<Up>"] = cmp.mapping.select_prev_item({ behavior = "select" }),
+            ["<Down>"] = cmp.mapping.select_next_item({ behavior = "select" }),
+            ["<C-Space>"] = cmp.mapping(function(fallback)
+              if cmp.visible() then
+                cmp.abort()
+              else
+                cmp.complete()
+              end
+            end),
+            ["<Tab>"] = cmp.mapping(function(fallback)
+              if cmp.visible() then
+                cmp.select_next_item({ behavior = "insert" })
+                -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
+                -- that way you will only jump inside the snippet region
+              elseif luasnip.expand_or_jumpable() then
+                luasnip.expand_or_jump()
+              elseif has_words_before() then
+                cmp.complete()
+              else
+                fallback()
+              end
+            end, { "i", "s" }),
 
-						["<S-Tab>"] = cmp.mapping(function(fallback)
-							if cmp.visible() then
-								cmp.select_prev_item({ behavior = "insert" })
-							elseif luasnip.jumpable(-1) then
-								luasnip.jump(-1)
-							else
-								fallback()
-							end
-						end, { "i", "s" }),
-					},
-				})
-			end,
-		},
-		{ "hrsh7th/cmp-buffer" },
-		{ "hrsh7th/cmp-path" },
-		{ "saadparwaiz1/cmp_luasnip" },
-		{ "hrsh7th/cmp-nvim-lsp" },
-		{ "hrsh7th/cmp-nvim-lua" },
-		{
-			"windwp/nvim-autopairs",
-			config = function()
-				require("nvim-autopairs").setup({})
-			end,
-		},
+            ["<S-Tab>"] = cmp.mapping(function(fallback)
+              if cmp.visible() then
+                cmp.select_prev_item({ behavior = "insert" })
+              elseif luasnip.jumpable(-1) then
+                luasnip.jump(-1)
+              else
+                fallback()
+              end
+            end, { "i", "s" }),
+          },
+        })
+      end,
+    },
+    { "hrsh7th/cmp-buffer" },
+    { "hrsh7th/cmp-path" },
+    { "saadparwaiz1/cmp_luasnip" },
+    { "hrsh7th/cmp-nvim-lsp" },
+    { "hrsh7th/cmp-nvim-lua" },
+    {
+      "windwp/nvim-autopairs",
+      config = function()
+        require("nvim-autopairs").setup({})
+      end,
+    },
 
-		-- Snippets
-		{
-			"L3MON4D3/LuaSnip",
-			dependencies = { "rafamadriz/friendly-snippets" },
-		},
-		{
-			"onsails/lspkind.nvim",
-		},
-	},
-	config = function()
-		require("neodev").setup()
-		require("mason").setup()
-		require("mason-lspconfig").setup({
-			ensure_installed = {
-				"lua_ls",
-				"gopls",
-				"tsserver",
-				"eslint",
-				"rust_analyzer",
-				"clangd",
-				"dockerls",
-				"docker_compose_language_service",
-				"tailwindcss",
-				"vimls",
-			},
-			automatic_insallation = true,
-		})
-		local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-		local cmp = require("cmp")
+    -- Snippets
+    {
+      "L3MON4D3/LuaSnip",
+      dependencies = { "rafamadriz/friendly-snippets" },
+    },
+    {
+      "onsails/lspkind.nvim",
+    },
+  },
+  config = function()
+    require("neodev").setup()
+    require("mason").setup()
+    require("mason-lspconfig").setup({
+      ensure_installed = {
+        "lua_ls",
+        "gopls",
+        "tsserver",
+        "eslint",
+        "rust_analyzer",
+        "clangd",
+        "dockerls",
+        "docker_compose_language_service",
+        "tailwindcss",
+        "vimls",
+      },
+      automatic_insallation = true,
+    })
+    local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+    local cmp = require("cmp")
 
-		local capabilities = require("cmp_nvim_lsp").default_capabilities()
+    local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-		require("mason-lspconfig").setup_handlers({
-			function(server_name)
-				require("lspconfig")[server_name].setup({
-					capabilities = capabilities,
-				})
-			end,
-		})
+    require("mason-lspconfig").setup_handlers({
+      function(server_name)
+        require("lspconfig")[server_name].setup({
+          capabilities = capabilities,
+        })
+      end,
+    })
 
-		-- Keymaps for LSP actions
-		local builtin = require("telescope.builtin")
+    -- Keymaps for LSP actions
+    local builtin = require("telescope.builtin")
 
-		-- Native LSP bindings
-		vim.keymap.set("n", "gh", vim.lsp.buf.hover)
-		vim.keymap.set("n", "gl", vim.diagnostic.open_float)
-		vim.keymap.set("n", "gD", vim.lsp.buf.declaration)
-		vim.keymap.set("n", "ga", vim.lsp.buf.code_action)
+    -- Native LSP bindings
+    vim.keymap.set("n", "gh", vim.lsp.buf.hover)
+    vim.keymap.set("n", "gl", vim.diagnostic.open_float)
+    vim.keymap.set("n", "gD", vim.lsp.buf.declaration)
+    vim.keymap.set("n", "ga", vim.lsp.buf.code_action)
 
-		-- Telescope builtins
-		vim.keymap.set("n", "gd", builtin.lsp_definitions, {})
-		vim.keymap.set("n", "gt", builtin.lsp_type_definitions, {})
-		vim.keymap.set("n", "gr", builtin.lsp_references, {})
-		vim.keymap.set("n", "gi", builtin.lsp_implementations, {})
+    -- Telescope builtins
+    vim.keymap.set("n", "gd", builtin.lsp_definitions, {})
+    vim.keymap.set("n", "gt", builtin.lsp_type_definitions, {})
+    vim.keymap.set("n", "gr", builtin.lsp_references, {})
+    vim.keymap.set("n", "gi", builtin.lsp_implementations, {})
 
-		-- Telescope diagnostics
-		vim.keymap.set("n", "<leader>dd", function() -- Buffer diagnostics
-			builtin.diagnostics({ bufnr = 0 })
-		end)
-		vim.keymap.set("n", "<leader>dw", builtin.diagnostics, {}) -- Workspace diagnostics
+    -- Telescope diagnostics
+    vim.keymap.set("n", "<leader>dd", function() -- Buffer diagnostics
+      builtin.diagnostics({ bufnr = 0 })
+    end)
+    vim.keymap.set("n", "<leader>dw", builtin.diagnostics, {}) -- Workspace diagnostics
 
-		vim.keymap.set("n", "<leader>ni", "<cmd>LspInfo<CR>")
+    vim.keymap.set("n", "<leader>ni", "<cmd>LspInfo<CR>")
 
-		-- Auto insert parenthesis after function or method completion
-		cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+    -- Auto insert parenthesis after function or method completion
+    cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
 
-		-- Sign column icons
-		local function set_sign_icons(opts)
-			opts = opts or {}
+    -- Sign column icons
+    local function set_sign_icons(opts)
+      opts = opts or {}
 
-			local sign = function(args)
-				if opts[args.name] == nil then
-					return
-				end
+      local sign = function(args)
+        if opts[args.name] == nil then
+          return
+        end
 
-				vim.fn.sign_define(args.hl, {
-					texthl = args.hl,
-					text = opts[args.name],
-					numhl = "",
-				})
-			end
+        vim.fn.sign_define(args.hl, {
+          texthl = args.hl,
+          text = opts[args.name],
+          numhl = "",
+        })
+      end
 
-			sign({ name = "error", hl = "DiagnosticSignError" })
-			sign({ name = "warn", hl = "DiagnosticSignWarn" })
-			sign({ name = "hint", hl = "DiagnosticSignHint" })
-			sign({ name = "info", hl = "DiagnosticSignInfo" })
-		end
+      sign({ name = "error", hl = "DiagnosticSignError" })
+      sign({ name = "warn", hl = "DiagnosticSignWarn" })
+      sign({ name = "hint", hl = "DiagnosticSignHint" })
+      sign({ name = "info", hl = "DiagnosticSignInfo" })
+    end
 
-		set_sign_icons({
-			error = "✘",
-			warn = "▲",
-			hint = "⚑",
-			info = "»",
-		})
-	end,
+    set_sign_icons({
+      error = "✘",
+      warn = "▲",
+      hint = "⚑",
+      info = "»",
+    })
+  end,
 }
